44e1ff7843381a3795ee4784491830bc44876ea2
Dragan Zaric <dobrivoje.topalovic@gmail.com>
2019-05-16 14:19:58 +0200

    Add info block and 3 columns for images
    Create some frontend blocks for app

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,7 +13,22 @@
           <div class="pr-4"><strong>50</strong> followers</div>
           <div class="pr-4"><strong>212</strong> following</div>
           </div>
-      </div><!-- col-9 -->
+          <div class="pt-4 font-weight-bold">dezareo.me</div>
+          <div>We're a global community of milions of people learning to code together. We're an
+               open source, donor-supported, non profit organization.</div>
+          <div><a href="#">dezareo.me</a></div>
+      </div><!-- /col-9 -->
+    </div><!-- /row -->
+    <div class="row pt-4">
+      <div class="col-4">
+        <img src="https://instagram.fbeg5-1.fna.fbcdn.net/vp/d53f9597a3831039fd161fc14b60c3f7/5D7435C9/t51.2885-15/sh0.08/e35/s640x640/58714803_2255707798080526_7541083713673676840_n.jpg?_nc_ht=instagram.fbeg5-1.fna.fbcdn.net" class="w-100">
+      </div>
+      <div class="col-4">
+        <img src="https://instagram.fbeg5-1.fna.fbcdn.net/vp/6ea9c698f17b899530103991346145a8/5D6BAE4D/t51.2885-15/e35/s480x480/23733777_1611929602201255_7510853573895258112_n.jpg?_nc_ht=instagram.fbeg5-1.fna.fbcdn.net" class="w-100">
+      </div>
+      <div class="col-4">
+        <img src="https://instagram.fbeg5-1.fna.fbcdn.net/vp/3c7bf2b8ced7467916e2e7c13d6cbc8d/5D7A111B/t51.2885-15/e35/s320x320/18888953_118163858772873_7019865831878164480_n.jpg?_nc_ht=instagram.fbeg5-1.fna.fbcdn.net" class="w-100">
+      </div>
+
     </div><!-- row -->
-</div>
 @endsection
