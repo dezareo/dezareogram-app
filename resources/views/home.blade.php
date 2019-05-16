4b0e6590992c8f8ad14498595c871ea82ce37e12
Dragan Zaric <dobrivoje.topalovic@gmail.com>
2019-05-16 13:37:46 +0200

    Make changes in dashboard section
    Make changes in dashboard section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,22 +2,18 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-    </div>
+    <div class="row">
+      <div class="col-3 p-5">
+        <img src="/svg/second-image.jpg" class="rounded-circle" style="height: 120px;">
+      </div><!-- col-3 -->
+      <div class="col-9 pt-5">
+          <div><h1>Dezareogram</h1></div>
+          <div class="d-flex">
+          <div class="pr-4"><strong>153</strong> posts</div>
+          <div class="pr-4"><strong>50</strong> followers</div>
+          <div class="pr-4"><strong>212</strong> following</div>
+          </div>
+      </div><!-- col-9 -->
+    </div><!-- row -->
 </div>
 @endsection
